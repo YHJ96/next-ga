dd9e1d191a109c5b07da61c7758ec51066b96d82
양희준 <88187322+YHJ96@users.noreply.github.com>
2023-02-23 17:52:06 +0900

    feat: 타입 지정 완료

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,14 +1,32 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
+interface GoogleEventParams {
+  action: string;
+  category: string;
+  label: string;
+  value: string | number;
+}
+
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
+export const pageview = (url: string) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: GoogleEventParams) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
